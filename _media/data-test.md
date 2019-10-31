d036a8319f9a271981307cc1109324d892b1c1e5
Justin Slack <justin@newmedialabs.co.za>
2019-10-31 12:58:54 +0200

    Add data test

---
+++ _media/data-test.md
@@ -0,0 +1,14 @@
+---
+title: Eskom debt
+layout: data-post
+category: standard
+date: 02 March 2018
+featured-image: 
+thumbnail: /assets/images/budget-one.png
+alt: Progressive tax
+includejs: eskom-debt.js
+---
+
+<div id="container"></div>
+<div id="customLegend"></div>
+
---
+++ scripts/data/eskom-debt.js
@@ -0,0 +1,52 @@
+Highcharts.chart('container', {
+
+  chart: {
+    style: {
+      fontFamily: 'Montserrat'
+    }
+  },
+
+  title: {
+    text: 'Eskom debt over time'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Millions of Rand'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+        },
+      pointStart: 2004
+    }
+  },
+
+  tooltip: {
+    pointFormat: '<span>{series.name}</span>: <b>R{point.y}m</b>',
+    valueDecimals: 0,
+    split: true,
+    shadow: false
+  },
+
+  series: [{
+    color : '#eb667c',
+    name: 'Eskom debt',
+    data: [156,	143, 133, 135, 147, 46678, 67057, 77230, 103523, 125125, 149944, 174586, 218199, 242984, 264843, 283542]
+  }],
+
+  responsive: {
+    rules: [{
+      condition: {
+        maxWidth: 500
+      }
+  }]
+ }
+
+});
